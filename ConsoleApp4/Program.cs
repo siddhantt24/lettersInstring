08037d543a60a191dfb91d1f334ff6562ceeaf00
Siddhant Tandon <76514713+siddhantt24@users.noreply.github.com>
2024-07-26 01:46:43 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Exercise5
+{
+    
+    public static void Main()
+    {
+        string str; 
+        int l, wrd; 
+
+        Console.Write("Input the string: ");
+        str = Console.ReadLine(); 
+
+        l = 0; 
+        wrd = 1;
+
+        
+        while (l <= str.Length - 1)
+        {
+        
+            if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+            {
+                wrd++; 
+            }
+
+            l++;
+        }
+
+        
+        Console.Write("Total number of words in the string is: {0}\n", wrd);
+    }
+}
